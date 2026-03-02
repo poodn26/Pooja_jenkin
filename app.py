74e8d5353950285f9c59066dfe03f363cb595377
poodn26 <poodn26@gmail.com>
2026-03-02 14:26:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("hello world")
+num = 5
+
+factorial = 1
+
+if num < 0:
+    print("Factorial does not exist for negative numbers")
+elif num == 0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1, num + 1):
+        factorial *= i
+    print("Factorial of", num, "is", factorial)
